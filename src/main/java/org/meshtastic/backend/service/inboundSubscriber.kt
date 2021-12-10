5adaa1645b65da7242c11cb2a650f8f66f1a58f2
bnazari <324817+bnazari@users.noreply.github.com>
2021-12-10 18:06:45 -0500

    Update inboundSubscriber.kt

--- src/main/java/org/meshtastic/backend/service/inboundSubscriber.kt
+++ src/main/java/org/meshtastic/backend/service/inboundSubscriber.kt
@@ -12,50 +12,53 @@ import org.meshtastic.backend.model.ChannelDB
 
 @Component
 class inboundSubscriber(
-        private val configuration: Configuration,
-        private val channels: ChannelDB
-        ) :
+	private val configuration: Configuration,
+	private val channels: ChannelDB
+	) :
     MQTTSubscriber("${configuration.inboundRoot}#") {
     private val logger = KotlinLogging.logger {}
+   var counter: Int = 1 
     override fun onTopicReceived(msg: MqttMessage) {
-        logger.debug(String(msg.payload))
-         val p = MeshProtos.MeshPacket.newBuilder().apply {
-                id = (100000000).toInt()
-                from = (-9999999).toInt()
+	logger.debug(String(msg.payload))
+	 val p = MeshProtos.MeshPacket.newBuilder().apply {
+                id = counter
+                from = (-9999999999).toInt() 
                 to = 0xffffffff.toInt() // ugly way of saying broadcast
+		hopLimit = 3
+		channel = 177
                 rxTime = (System.currentTimeMillis() / 1000).toInt()
                 rxSnr = 1.5f
                 decoded = MeshProtos.Data.newBuilder().apply {
                     portnum = Portnums.PortNum.TEXT_MESSAGE_APP
                     payload = ByteString.copyFromUtf8(String(msg.payload))
                 }.build()
-        }.build()
-        var e =  MQTTProtos.ServiceEnvelope.newBuilder().apply{
-                packet = p
-                channelId = "LongSlow"
-                gatewayId = "!99999999"
-                }.build()
-        if(e.hasPacket()){
-                logger.info("Has packet")
-        }
-        if(e.packet.payloadVariantCase == MeshProtos.MeshPacket.PayloadVariantCase.DECODED){
-                logger.info("Decrypted")
-        }
+	}.build()
+	counter = counter + 1
+	var e =  MQTTProtos.ServiceEnvelope.newBuilder().apply{
+		packet = p
+		channelId = "LongSlow"
+		gatewayId = "!%08x".format(p.from)
+		}.build()
         val nodeId = "!%08x".format(e.packet.from)
-        val Topic = "msh/dev/clear/LongSlow/${nodeId}/1"
-        mqtt.publish(Topic, e.toByteArray())
+        val Topic = "msh/1/c/LongSlow/${nodeId}"
+//        mqtt.publish(Topic, e.toByteArray())
 
             val ch = channels.getById(e.channelId)
             if (ch == null)
                 logger.warn("Topic $topic, channel not found")
             else {
-                logger.info("Let's encrypt")
                 val psk = ch.psk.toByteArray()
-                val encrypted = encrypt(psk, p.from, p.id, p.decoded.toByteArray())
-                val encoded = MeshProtos.Data.parseFrom(encrypted)
-//              val encodedPacket = p.toBuilder().clearDecoded().setEncrypted(encrypted).build()
-//              e.toBuilder().setPacket(encodedPacket).build()
-                }
+                val encrypted = ByteString.copyFrom(encrypt(psk, p.from, p.id, p.decoded.toByteArray()))
+		val encodedPacket = p.toBuilder().clearDecoded().setEncrypted(encrypted).build()
+		var j =  MQTTProtos.ServiceEnvelope.newBuilder().apply{
+                	packet = encodedPacket
+                	channelId = "LongSlow"
+                	gatewayId = "!%08x".format(p.from)
+                }.build()
 
-        }
+ 		e.toBuilder().setPacket(encodedPacket).build()
+ 		mqtt.publish(Topic, j.toByteArray())		
+		}
+	
+	}	
 }
